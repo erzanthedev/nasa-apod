41b59c362d2732161928ad14d6fb34849c52bd6c
Michael Erzan Andoh <erzanthedev@gmail.com>
2024-07-02 16:51:14 +1000

    feat: added functon to fetch NASA API

---
+++ script.js
@@ -0,0 +1,19 @@
+// NASA API
+const count = 10;
+const apiKey = "DEMO_KEY";
+const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
+
+let resultsArray = [];
+
+const getNasaPictures = async () => {
+  try {
+    const response = await fetch(apiUrl);
+    resultsArray = await response.json();
+    console.log(resultsArray);
+  } catch (error) {
+    console.log("Wooops, error here: ", error);
+  }
+};
+
+// On load
+getNasaPictures();
